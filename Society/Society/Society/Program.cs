d68d7a498662d335dda2d2b6abc814e5a7bb3d48
jgd <jgd@netcompany.com>
2023-11-26 19:47:35 +0100

    initial commit

---
+++ Society/Society/Society/Program.cs
@@ -0,0 +1,39 @@
+using BlazorApp1;
+using BlazorApp1.Client.Pages;
+using BlazorApp1.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Register the service
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Clientside).Assembly);
+
+app.Run();
