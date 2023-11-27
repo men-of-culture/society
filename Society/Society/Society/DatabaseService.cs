0fe1d3a9e0e4838f029df005f3494e36ce3e2de6
jgd <jgd@netcompany.com>
2023-11-27 01:08:00 +0100

    you can clear the table and cleaned up

--- Society/Society/Society/DatabaseService.cs
+++ Society/Society/Society/DatabaseService.cs
@@ -8,6 +8,7 @@ namespace Society
     {
         Task<List<string>> GetStringsFromDb();
         Task AddStringToDb(string value);
+        Task ClearDb();
     }
     public class DatabaseService: IDatabaseService
     {
@@ -17,20 +18,18 @@ namespace Society
         {
             var data = new List<string>();
 
-            using MySqlConnection connection = new MySqlConnection(connectionString);
+            using MySqlConnection connection = new MySqlConnection(connectionString); // prepare connection
             await connection.OpenAsync();
 
-            using MySqlCommand command = new MySqlCommand("SELECT MyColumn FROM MyTable", connection);
-            using MySqlDataReader reader = await command.ExecuteReaderAsync();
+            using MySqlCommand command = new MySqlCommand("SELECT MyColumn FROM MyTable", connection); // prepare command
+            using MySqlDataReader reader = await command.ExecuteReaderAsync(); // execute command
 
             while (await reader.ReadAsync())
             {
-                // Retrieve data from the column
-                string value = reader.GetString(0);
+                string value = reader.GetString(0); // Retrieve data from the column
 
                 data.Add(value);
             }
-            data.Add("Freezing");
             await connection.CloseAsync();
 
             return data;
@@ -41,6 +40,15 @@ namespace Society
             await connection.OpenAsync();
 
             using MySqlCommand command = new MySqlCommand("INSERT INTO MyTable (MyColumn) VALUES (" + value + ")", connection);
+            using MySqlDataReader reader = await command.ExecuteReaderAsync();
+            await connection.CloseAsync();
+        }
+        public async Task ClearDb()
+        {
+            using MySqlConnection connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            using MySqlCommand command = new MySqlCommand("TRUNCATE MyTable", connection);
             using MySqlDataReader reader = await command.ExecuteReaderAsync();
             await connection.CloseAsync();
         }
