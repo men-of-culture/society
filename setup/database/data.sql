28ef09673e6d5675fcda1f4add4e1b2dc88fa8c0
Martin <zyjinxx@hotmail.com>
2023-11-28 17:36:53 +0100

    database, docker.compose: Adds table creation and data script to insert dummy data into tables, gives database container a name

---
+++ setup/database/data.sql
@@ -0,0 +1,51 @@
+USE Society;
+GO
+
+DECLARE @counter INT = 1;
+DECLARE @friendCounter int = 1;
+
+WHILE @counter <= 100
+BEGIN
+    DECLARE @guid UNIQUEIDENTIFIER = NEWID();
+    INSERT INTO [dbo].[User] ([Id], [Email], [Password], [Name], [Description], [Image])
+    VALUES (
+		@guid,
+		'user' + CONVERT(VARCHAR(10), @counter) + '@example.com',
+		'fakePasswordNeedsToBeSha256',
+		'user' + CONVERT(VARCHAR(10), @counter),
+		REPLICATE('Very long description', 10),
+		'');
+    SET @counter = @counter + 1;
+END;
+
+DECLARE @value UNIQUEIDENTIFIER
+DECLARE db_cursor CURSOR FOR
+SELECT Id FROM [dbo].[User]
+OPEN db_cursor
+FETCH NEXT FROM db_cursor INTO @value
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    DECLARE @friendId UNIQUEIDENTIFIER
+    DECLARE db_cursor1 CURSOR FOR
+    SELECT top 20 percent Id FROM [dbo].[User] WHERE Id != @value ORDER BY NEWID()
+    OPEN db_cursor1
+    FETCH NEXT FROM db_cursor1 INTO @friendId
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        INSERT INTO [dbo].[Friend] ([UserId], [FriendId], [FriendName], [FriendImage])
+        VALUES (
+            @value,
+            @friendId,
+            'user' + CONVERT(VARCHAR(MAX), @friendId),
+            '');
+        FETCH NEXT FROM db_cursor1 INTO @friendId
+    END;
+    CLOSE db_cursor1;
+    DEALLOCATE db_cursor1;
+
+    FETCH NEXT FROM db_cursor INTO @value
+END;
+CLOSE db_cursor;
+DEALLOCATE db_cursor;
--- setup/database/init.sql
+++ setup/database/init.sql
@@ -1,11 +1,37 @@
-USE master
+USE [master]
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'MyTestDataBase')
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Society')
 BEGIN
-  CREATE DATABASE MyTestDataBase;
+  CREATE DATABASE [Society];
 END;
 GO
 
-USE MyTestDataBase;
+USE [Society];
+GO
+
+DROP TABLE IF EXISTS [dbo].[Friend];
+GO
+
+DROP TABLE IF EXISTS [dbo].[User];
+GO
+
+CREATE TABLE [dbo].[User] (
+  [Id] UNIQUEIDENTIFIER PRIMARY KEY,
+  [Email] NVARCHAR(100),
+  [Password] NVARCHAR(100),
+  [Name] NVARCHAR(100),
+  [Description] NVARCHAR(MAX),
+  [Image] IMAGE
+);
+GO
+
+CREATE TABLE [dbo].[Friend] (
+  [UserId] UNIQUEIDENTIFIER,
+  [FriendId] UNIQUEIDENTIFIER,
+  [FriendName] NVARCHAR(100),
+  [FriendImage] IMAGE
+  FOREIGN KEY ([UserId]) REFERENCES [dbo].[User](Id),
+  FOREIGN KEY ([FriendId]) REFERENCES [dbo].[User](Id),
+);
 GO
