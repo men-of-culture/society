48a29048c3a5b30d8849e87669d9339554b435a2
jgd <jgd@netcompany.com>
2023-12-04 02:33:49 +0100

    split my user profile and friend user profile in two

--- Society/Society/Society/Components/SharedClientVars.cs
+++ Society/Society/Society/Components/SharedClientVars.cs
@@ -6,6 +6,7 @@
         {
             //string API_URL = "https://localhost:7079/";
             public static string API_URL = "http://localhost:5085/";
+            public static string profileGuid = "invalid";
         }
     }
 }
