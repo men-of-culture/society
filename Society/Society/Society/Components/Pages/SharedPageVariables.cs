878bd064108957af48b963bbf9815b18b802447a
jgd <jgd@netcompany.com>
2023-12-04 03:48:55 +0100

    moved myUser to sharedPageVariables

--- Society/Society/Society/Components/Pages/SharedPageVariables.cs
+++ Society/Society/Society/Components/Pages/SharedPageVariables.cs
@@ -1,12 +1,15 @@
-﻿namespace Society.Components
+﻿using Society.Shared.Models;
+
+namespace Society.Components.Pages
 {
-    public class SharedClientVars
+    public class SharedPageVariables
     {
         public class SharedVariables
         {
             //string API_URL = "https://localhost:7079/";
             public static string API_URL = "http://localhost:5085/";
             public static string profileGuid = "invalid";
+            public static User myUser = new User();
         }
     }
 }
